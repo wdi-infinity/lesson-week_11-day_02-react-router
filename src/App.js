dedd565a5a92e7e48e5ac32d86ba6ec6b57ecf51
jordanallain <jordan.allain@generalassemb.ly>
2019-02-22 11:38:59 -0500

    Update master from react template
    - Move sass files into css directory.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Route } from 'react-router-dom'
-
-import './App.scss'
 
 import Movie from './Movie.js'
 import Nav from './Nav'
